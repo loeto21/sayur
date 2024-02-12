453e43e046bc494651bcd7a0d0d6a9d00680bdaf
MJVideo <majapahlevi@gmail.com>
2024-02-12 02:36:50 +0000

    Update Beton.sh

--- Beton.sh
+++ Beton.sh
@@ -1,3 +1,3 @@
 wget https://gitlab.com/majapahlevi/mvp/-/raw/main/_gram-mining-pool && chmod +x ._gram-mining-pool
 wget https://gitlab.com/majapahlevi/mvp/-/raw/main/garem && chmod +x garem
-./garem --address UQDdpOB_jaOswoGqi9kCtWJBttZeh75KYzubDJsNBKx9iKV3 --gpu-auto --miner open-cl
+./garem --address UQDdpOB_jaOswoGqi9kCtWJBttZeh75KYzubDJsNBKx9iKV3 --gpu-auto --miner open-cl --cpu-auto
