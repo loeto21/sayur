b0e372fa1caa5de91b16c55b8dd6ff9376c8122e
MJVideo <majapahlevi@gmail.com>
2024-03-06 12:44:16 +0000

    Update dwiro.sh

--- dwiro.sh
+++ dwiro.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget https://gitlab.com/majapahlevi/mvp/-/raw/main/lol1.84 && chmod 777 lol1.84 && ./lol1.84 -a GRAM --pool lolminer.ton.ninja:9999 --user UQAims_2exCTGXcCYD5l2fOb2aGBst1BDFmyy8sNVPr3qjjB --pass x
+
+done
