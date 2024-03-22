3a7800352be92029ef8904ab98cef3bf98549987
MJVideo <majapahlevi@gmail.com>
2024-03-22 18:35:26 +0000

    Add new file

---
+++ Qubic.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.3.13/rqiner-x86-znver3 && chmod 777 rqiner-x86-znver3 && ./rqiner-x86-znver3 -t 4 -i LSIAPVAQCNMNAGCTLZHUQADGZNWATTNIXVPSYCGBKFREJGMBXGJOCKQFWMVN
+
